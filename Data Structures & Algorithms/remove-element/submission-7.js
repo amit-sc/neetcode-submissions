aa8ad87d995557430c3b478ed734f12b650caabb
Amit Shree Chandan <52486291+amit-sc@users.noreply.github.com>
2026-06-22 22:29:32 +0530

    Add: remove-element - submission-7

---
+++ Data Structures & Algorithms/remove-element/submission-7.js
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        let newArr = nums.filter(ele => ele !== val)
+        nums.splice(0, 0, ...newArr)
+        return newArr.length;
+    }
+}
