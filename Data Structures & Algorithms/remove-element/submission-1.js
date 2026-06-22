fc780c1f53a27bd3bae88098f6c56217a0c0db48
Amit Shree Chandan <52486291+amit-sc@users.noreply.github.com>
2026-06-22 22:02:30 +0530

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        let newArr = nums.filter(ele => ele !== val)
+
+        let n = nums.length
+        let n2 = newArr.length
+
+        nums.length = 0
+
+        for(let ele of newArr){
+            nums.push(ele)
+        }
+
+        nums.push(...Array(n-n2).fill(val))
+
+
+        // let n = newArr.length
+        // let len = nums.length - newArr.length
+
+        // for(let i = 0; i < len; i++)
+        // {
+        //     newArr.push(val)
+        // }
+
+        // nums.length = 0
+        // for(let ele of newArr){
+        //     nums.push(ele)
+        // }
+
+        return n2;
+    }
+}
