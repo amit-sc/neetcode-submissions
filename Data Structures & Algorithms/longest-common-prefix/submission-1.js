881bfbed0cd6023a050de6de6965385e9bf03818
Amit Shree Chandan <52486291+amit-sc@users.noreply.github.com>
2026-06-23 10:28:31 +0530

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        let min_str = strs.reduce((acc, ele) => {
+            if(acc.length < ele.length){
+                return acc
+            }
+            return ele
+        })
+
+        let res = ""
+        let result = ""
+        for(let i = 0; i < min_str.length; i++){
+            res += min_str[i]
+            if(strs.every(ele => ele.includes(res))){
+               result = res 
+            }
+        }
+
+        return result
+
+    }
+}
