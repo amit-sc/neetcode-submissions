83d153ffffcce252a6f89386b6f0b289aab5f524
Amit Shree Chandan <52486291+amit-sc@users.noreply.github.com>
2026-06-30 22:23:45 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number} x
+     * @param {number} n
+     * @return {number}
+     */
+    myPow(x, n) {
+        if(n == 0) return 1
+
+        let res = 1
+        for(let i = 0; i < Math.abs(n); i++){
+            res = res * x
+        }
+        
+        if(n < 0) return 1/res
+        else return res
+        
+
+    }
+}
